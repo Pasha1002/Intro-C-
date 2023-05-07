860ab60d662295dcdc1d57be90e01ab796c4cbcb
Pasha1002 <kanavetspavel2001@gmail.com>
2023-05-07 13:03:46 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+using std::endl; 
+using std::cout;
+
+
+int main()
+{
+	cout << "Name       : 'The war and the peace'\n";
+	cout << "Avtor      : L.N. Tolstoj" << endl;
+	cout << "Izdatelstvo: Piter" << endl;
+	cout << "Pages      : 500." << endl;
+	system("pause");
+	return 0;
+}
